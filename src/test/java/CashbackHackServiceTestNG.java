d31b90304281ee277e923b1e2e7fb40439e79eac
LessiaSa <0162221@mail.ru>
2024-08-07 20:36:23 +0700

    NewTestsJunit4

--- src/test/java/CashbackHackServiceTestNG.java
+++ src/test/java/CashbackHackServiceTestNG.java
@@ -1,7 +1,10 @@
-import org.testng.annotations.Test;
+
+import org.junit.Assert;
+import org.junit.Test;
 import ru.netology.javaqa.CashbackHackService;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTestNG {
     CashbackHackService service = new CashbackHackService();
@@ -11,43 +14,41 @@ public class CashbackHackServiceTestNG {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual,expected,"");
+       assertEquals(expected,actual);
     }
     @Test
     public void testNormItems() {
         int amount = 483;
         int actual = service.remain(amount);
         int expected = 517;
-        assertEquals(actual,expected,"");
+        assertEquals(expected,actual);
     }
     @Test
     public void testLimitValueMax() {
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-        assertEquals(actual,expected,"");
+        assertEquals(expected,actual);
     }
     @Test
     public void testLimitValueMin() {
         int amount = 1;
         int actual = service.remain(amount);
         int expected = 999;
-        assertEquals(actual,expected,"");
+        assertEquals(expected,actual);
     }
     @Test
     public void testSum1000() {
         int amount = 1000;
         int actual =service.remain(amount);
         int expected = 1000;
-        assertEquals(actual,expected,"");
+        assertEquals(expected,actual);
     }
     @Test
     public void testMoreThanA1000() {
         int amount = 1001;
         int actual =service.remain(amount);
         int expected = 999;
-        assertEquals(actual,expected,"");
+        assertEquals(expected,actual);
     }
-
-
 }
