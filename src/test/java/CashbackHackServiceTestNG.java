fc18fe19fe616f75c3475eee13cf0635d984aac9
LessiaSa <0162221@mail.ru>
2024-08-08 08:27:16 +0700

    Update CashbackHackServiceTestNG.java

--- src/test/java/CashbackHackServiceTestNG.java
+++ src/test/java/CashbackHackServiceTestNG.java
@@ -38,7 +38,7 @@ public class CashbackHackServiceTestNG {
     public void testSum1000() {
         int amount = 1000;
         int actual =service.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(actual,expected,"");
     }
     @Test
